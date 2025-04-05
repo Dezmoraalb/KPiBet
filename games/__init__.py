138a8b58c2b3d541df8253921f7859c429573b21
dezmoraalb <pasha34523452@gmail.com>
2025-04-05 10:14:22 +0300

    add __init__.py and update runner init test

---
+++ games/__init__.py
@@ -0,0 +1,7 @@
+from games.dice_game import DiceGame
+from games.rps_game import RockPaperScissorsGame
+
+__all__ = [
+    "DiceGame",
+    "RockPaperScissorsGame"
+]
--- run_tests.py
+++ run_tests.py
@@ -3,22 +3,17 @@ import sys
 import os
 
 if __name__ == "__main__":
-    # Настраиваем путь для импорта модулей проекта
     sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-    
-    # Загружаем все тесты из директории tests
+
     test_loader = unittest.TestLoader()
     test_suite = test_loader.discover('tests')
-    
-    # Запускаем тесты
+
     test_runner = unittest.TextTestRunner(verbosity=2)
     test_result = test_runner.run(test_suite)
-    
-    # Выводим итоговую информацию
+
     print(f"\nВсього тестiв: {test_result.testsRun}")
     print(f"Успiшно: {test_result.testsRun - len(test_result.failures) - len(test_result.errors)}")
     print(f"Помилок: {len(test_result.errors)}")
     print(f"Провальних: {len(test_result.failures)}")
-    
-    # Устанавливаем код возврата
+
     sys.exit(len(test_result.failures) + len(test_result.errors))
